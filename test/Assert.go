2de260ff14743416d10e8691797b5785f6c1b061
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2017-09-26 19:03:58 +0700

    [green] TestFindPatientByIdNotFound

--- test/Assert.go
+++ test/Assert.go
@@ -2,23 +2,41 @@ package test
 
 import "testing"
 import "strconv"
+import "reflect"
 import "github.com/DinoInc/BaseService"
 
-func AssertNil(t *testing.T, testName string, varName string, input interface{}){
-	if input != nil {
+func IsPointer(input interface{}) bool {
+	return reflect.ValueOf(input).Kind() == reflect.Ptr
+}
+
+func IsPointerNil(input interface{}) bool {
+	return reflect.ValueOf(input).IsNil()
+}
+
+func AssertNil(t *testing.T, testName string, varName string, input interface{}) {
+	isPointer := IsPointer(input)
+
+	if isPointer && !IsPointerNil(input) {
+		t.Error(testName + ", " + varName + " is not nil")
+	} else if !isPointer && input != nil {
 		t.Error(testName + ", " + varName + " is not nil")
 	}
 }
 
-func AssertNotNil(t *testing.T, testName string, varName string, input interface{}){
-	if input == nil {
-		t.Error(testName + ", " + varName + " is not nil")
+func AssertNotNil(t *testing.T, testName string, varName string, input interface{}) {
+	isPointer := IsPointer(input)
+
+	if isPointer && IsPointerNil(input) {
+		t.Error(testName + ", " + varName + " is nil")
+	} else if !isPointer && input == nil {
+		t.Error(testName + ", " + varName + " is nil")
 	}
+
 }
 
-func AssertCode(t *testing.T, testName string, err error, expectedCode int){
+func AssertCode(t *testing.T, testName string, err error, expectedCode int) {
 	var errorCode = err.(BaseService.Error).Code
-	if  errorCode != expectedCode {
-		t.Error(testName + ", err.Code = "+ strconv.Itoa(errorCode) +" not match, expected = "+ strconv.Itoa(expectedCode))
+	if errorCode != expectedCode {
+		t.Error(testName + ", err.Code = " + strconv.Itoa(errorCode) + " not match, expected = " + strconv.Itoa(expectedCode))
 	}
 }
