f32cbb8dcb44a511475adcd7cf398afe62fd4a04
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2017-09-26 20:23:00 +0700

    fix assertNil, assertNotNil bug

--- test/Assert.go
+++ test/Assert.go
@@ -5,30 +5,31 @@ import "strconv"
 import "reflect"
 import "github.com/DinoInc/BaseService"
 
-func IsPointer(input interface{}) bool {
-	return reflect.ValueOf(input).Kind() == reflect.Ptr
-}
+// reference: https://github.com/stretchr/testify
+func IsNil(input interface{}) bool {
 
-func IsPointerNil(input interface{}) bool {
-	return reflect.ValueOf(input).IsNil()
+	if input == nil {
+		return true
+	}
+
+	value := reflect.ValueOf(input)
+	kind := value.Kind()
+
+	// Is a [Chan, Func, Interface, Map, Ptr, Slice] and (reflect.Value).IsNil()
+	return (kind >= reflect.Chan && kind <= reflect.Slice) && value.IsNil()
+
 }
 
 func AssertNil(t *testing.T, testName string, varName string, input interface{}) {
-	isPointer := IsPointer(input)
 
-	if isPointer && !IsPointerNil(input) {
-		t.Error(testName + ", " + varName + " is not nil")
-	} else if !isPointer && input != nil {
+	if !IsNil(input) {
 		t.Error(testName + ", " + varName + " is not nil")
 	}
 }
 
 func AssertNotNil(t *testing.T, testName string, varName string, input interface{}) {
-	isPointer := IsPointer(input)
 
-	if isPointer && IsPointerNil(input) {
-		t.Error(testName + ", " + varName + " is nil")
-	} else if !isPointer && input == nil {
+	if IsNil(input) {
 		t.Error(testName + ", " + varName + " is nil")
 	}
 
