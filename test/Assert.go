41f02090462df8215bfbd75d8edf1f09bb8b6fe8
SatriaPriambada <satriapriambada@gmail.com>
2017-09-27 12:43:19 +0700

    refactor Assert.go

--- test/Assert.go
+++ test/Assert.go
@@ -7,16 +7,9 @@ import "github.com/DinoInc/BaseService"
 
 // reference: https://github.com/stretchr/testify
 func IsNil(input interface{}) bool {
-
-	if input == nil {
-		return true
-	}
-
+	
 	value := reflect.ValueOf(input)
-	kind := value.Kind()
-
-	// Is a [Chan, Func, Interface, Map, Ptr, Slice] and (reflect.Value).IsNil()
-	return (kind >= reflect.Chan && kind <= reflect.Slice) && value.IsNil()
+	return input == nil || value.IsNil()
 
 }
 
